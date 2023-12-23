b473bbecda4e00942a8de465991e5b873f3a30e2
thaoong <thao4985@gmail.com>
2023-12-23 15:26:11 +0700

    update sever

--- index.js
+++ index.js
@@ -14,7 +14,17 @@ app.use(express.urlencoded({ limit: '10mb' }));
 app.use(cookieParser());
 
 const cors = require("cors");
-app.use(cors())
+const allowedOrigins = ['http://localhost:4001', 'http://localhost:4002', 'https://beaurity-admin.vercel.app','https://beaurity-admin.netlify.app']
+app.use(cors({
+  origin: function(origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+          var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+          return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+  }
+}));
 app.listen(port, () => {
   console.log(`My Service start at port ${port}`)
 })
